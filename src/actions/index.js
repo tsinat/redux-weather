975633e5206653fe4abfc9890982eb08ec91f62b
Tsinat Zeree <cheru87@gmail.com>
2016-09-06 12:15:32 -0700

    added search bar

---
+++ src/actions/index.js
@@ -0,0 +1,1 @@
+const API_KEY = 'a4abb9a4b60a850e4d8cb5c4bb7d93a1';
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -11,9 +11,14 @@ export default class SearchBar extends Component {
         console.log(event.target.value);
         this.setState({term: event.target.value })
     }
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        //go and fetch weather data
+    }
     render() {
         return (
-            <form className='input-group'>
+            <form onSubmit={this.onFormSubmit} className='input-group'>
                 <input
                     placeholder="Get a five day foreast in your cities"
                     className='form-control'
