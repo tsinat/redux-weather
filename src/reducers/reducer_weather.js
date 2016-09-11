2e6362c7881d5540ed21e8e10a3e793a509f1fae
Tsinat Zeree <cheru87@gmail.com>
2016-09-11 11:41:16 -0700

    done with redux

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,7 +3,7 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action) {
     switch (action.type)  {
         case 'FETCH_WEATHER':
-            return state.push( action.payload.data );
+            return [action.payload.data, ...state];
     }
     return state;
 }
